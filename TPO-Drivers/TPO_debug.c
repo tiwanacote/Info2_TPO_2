b57fb65f04ef9216ffb066f65bddcf7ce308073d
tiwanacote <bertotto.maximiliano@gmail.com>
2017-11-01 10:20:52 -0300

    Mas motores agregados a la interrupción de boton

--- TPO-Drivers/TPO_debug.c
+++ TPO-Drivers/TPO_debug.c
@@ -13,8 +13,12 @@
 extern int pos_fin[8];
 extern int pos_ini[8];
 extern int is_moving;
-int buf [4]={100,150,1000}; //-90°,0°,90°
+//int buf [4]={100,150,1000}; //-90°,0°,90°
 //int buf_2 [4]={54,500,1000};
+int buf1 [6]={100,150,200,100,125,199}; //-90°,0°,90°
+int buf2 [6]={150,100,200,125,150,200}; //-90°,0°,90°
+int buf3 [6]={100,125,150,175,200,175}; //-90°,0°,90°
+int buf4 [6]={200,150,100,200,100,200}; //-90°,0°,90°
 
 
 void EINT3_IRQHandler( void )
@@ -24,10 +28,21 @@ void EINT3_IRQHandler( void )
 	static int contador = 0;
 	//printf ("HOLA/n");
 
-	pos_fin[5]=buf[contador];
-	//pos_ini[6]=buf_2[contador];
+	//pos_fin[5]=buf[contador];
+	//pos_ini[5]= pos_motor[5];
 
+	pos_fin[0]=buf1[contador];
+	pos_ini[0]= pos_motor[0];
+
+	pos_fin[1]=buf2[contador];
+	pos_ini[1]= pos_motor[1];
+
+	pos_fin[2]=buf3[contador];
+	pos_ini[2]= pos_motor[2];
+
+	pos_fin[5]=buf4[contador];
 	pos_ini[5]= pos_motor[5];
+
 
 	printf("BOTON presionado. Posicion: %d \n",pos_motor[5]);
 
@@ -35,7 +50,8 @@ void EINT3_IRQHandler( void )
 
 	contador++;
 
-	if(contador==3)
+	//if(contador==3)
+	if(contador==5)
 		contador=0;
 
 
